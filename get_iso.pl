c40518b74a4795f2b9d41c868836d27465e87526
Ying Peng <35216236+CongqianyouYz@users.noreply.github.com>
2018-10-03 15:12:38 +0800

    Create get_iso.pl

---
+++ get_iso.pl
@@ -0,0 +1,20 @@
+open FI, "Homo_sapiens.GRCh38.87.chr.gtf";
+open FO, ">knownIsoforms_to_sort.txt";
+
+while (<FI>) {
+	chomp;
+	next if (/^#/);
+
+	@line = split "\t";
+	$anno = $line[8];
+
+	($gene_id)			= $_ =~ /gene_id \"([^\"]+)\"\;/;
+	($transcript_id)	= $_ =~ /transcript_id \"([^\"]+)\"\;/;
+
+	print FO "$gene_id\t$transcript_id\n";
+}
+
+system "sort -V knownIsoforms_to_sort.txt | uniq > knownIsoforms.txt";
+
+close FI;
+close FO;
