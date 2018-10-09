2a05ea685f43f9d95fb74faebdb3ce723058b3df
Ying Peng <35216236+CongqianyouYz@users.noreply.github.com>
2018-10-09 20:59:41 +0800

    Update get_iso.pl

--- STAR_RSEM/get_iso.pl
+++ STAR_RSEM/get_iso.pl
@@ -1,17 +1,19 @@
-open FI, "Homo_sapiens.GRCh38.87.chr.gtf";
+open FI, "$ARGV[0]";
 open FO, ">knownIsoforms_to_sort.txt";
 
 while (<FI>) {
-	chomp;
-	next if (/^#/);
-    
-	@line = split "\t";
-	$anno = $line[8];
+    chomp;
+    next if (/^#/);
 
-	($gene_id)       = $_ =~ /gene_id \"([^\"]+)\"\;/;
-	($transcript_id) = $_ =~ /transcript_id \"([^\"]+)\"\;/;
+    @line = split "\t";
+    $anno = $line[8];
 
-	print FO "$gene_id\t$transcript_id\n";
+    ($gene_id)          = $_ =~ /gene_id \"([^\"]+)\"\;/;
+    ($transcript_id)    = $_ =~ /transcript_id \"([^\"]+)\"\;/;
+
+    if ($transcript_id) {
+        print FO "$gene_id\t$transcript_id\n";
+    }
 }
 
 system "sort -V knownIsoforms_to_sort.txt | uniq > knownIsoforms.txt";
