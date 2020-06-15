1243d9733358602a2fed15853e1ea18afe1ea7b8
Ying Peng <35216236+CongqianyouYz@users.noreply.github.com>
2020-06-15 11:12:46 +0800

    Update extract_gtf_genes.py

--- database_analysis/annos/extract_gtf_genes.py
+++ database_analysis/annos/extract_gtf_genes.py
@@ -1,24 +1,24 @@
 import sys, re
 
 gene_all = set()
-with open(sys.argv[1]) as fi:	# Homo_sapiens.GRCh38.88.chr.gtf
-	for line in fi:
-		seq = line.strip().split('\t')
-		if '#' in seq[0]:
-			continue
-		anno = seq[8]
-		try:
-			matchObj = re.match( r'.*gene_name "(.*?)".*gene_biotype "(.*?)".*', anno, re.M|re.I)
-			if matchObj.group(2) == 'protein_coding':
-				gene_all.add(re.sub(r'\.*', '', matchObj.group(1)))
-		except:
-			pass
-			"""
-			matchObj = re.match( r'gene_id "(.*?)".*gene_biotype "(.*?)".*', anno, re.M|re.I)
-			if matchObj.group(2) == 'protein_coding':
-				 gene_all.add(re.sub(r'\.*', '', matchObj.group(1)))
-			"""
+with open(sys.argv[1]) as fi:   # Homo_sapiens.GRCh38.88.chr.gtf
+    for line in fi: 
+        seq = line.strip().split('\t')
+        if '#' in seq[0]:
+            continue
+        anno = seq[8]
+        try:
+            matchObj = re.match( r'.*gene_name "(.*?)".*gene_biotype "(.*?)".*', anno, re.M|re.I)
+            if matchObj.group(2) == 'protein_coding':
+                gene_all.add(re.sub(r'\.*', '', matchObj.group(1)))
+        except:
+            pass
+            """
+            matchObj = re.match( r'gene_id "(.*?)".*gene_biotype "(.*?)".*', anno, re.M|re.I)
+            if matchObj.group(2) == 'protein_coding':
+                 gene_all.add(re.sub(r'\.*', '', matchObj.group(1)))
+            """
 fo = open('coding_genes.'+sys.argv[2], 'w')
 for gene in sorted(list(gene_all)):
-	fo.write(gene+'\n')
+    fo.write(gene+'\n')
 fo.close()
