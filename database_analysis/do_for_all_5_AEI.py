128320f660c3f326836d671ab0ba203ce9836e37
Ying Peng <35216236+CongqianyouYz@users.noreply.github.com>
2020-06-22 16:50:53 +0800

    Update do_for_all_5_AEI.py

--- database_analysis/do_for_all_5_AEI.py
+++ database_analysis/do_for_all_5_AEI.py
@@ -12,7 +12,7 @@ def Work(sprint_out_dir,a):
         subprocess.Popen('mkdir '+sprint_out_dir+'/AEI', shell=True).wait()
         subprocess.Popen(InstallPath+'/RNAEditingIndex -d /lustre/tianlab/a_pengying/hg38/'+sprint_out_dir+' -f ll.bam -l /lustre/tianlab/a_pengying/hg38/'+sprint_out_dir+'/AEI -o /lustre/tianlab/a_pengying/hg38/'+sprint_out_dir+'/AEI -os /lustre/tianlab/a_pengying/hg38/'+sprint_out_dir+'/AEI --genome hg38 --stranded --keep_cmpileup', shell=True).wait()
         subprocess.Popen('rm '+sprint_out_dir+'/AEI/ucscHg38Genome.fa.GenomeIndex.jsd', shell=True).wait()
-        subprocess.Popen('python /lustre/tianlab/a_pengying/scr/AEI_mine.py '+sprint_out_dir+'/AEI/a/a_ucscHg38Alu.bed.gz_mpileup_strand_1.cmpileup '+sprint_out_dir+'/AEI/a/a_ucscHg38Alu.bed.gz_mpileup_strand_2.cmpileup '+sprint_out_dir+'/A_to_I.res '+sprint_out_dir+'/aei.txt', shell=True).wait()
+        subprocess.Popen('python /lustre/tianlab/a_pengying/scr/AEI_mine.py '+sprint_out_dir+'/AEI/a/a_ucscHg38Alu.bed.gz_mpileup_strand_1.cmpileup '+sprint_out_dir+'/AEI/a/a_ucscHg38Alu.bed.gz_mpileup_strand_2.cmpileup '+sprint_out_dir+'/SPRINT_identified_regular.res '+sprint_out_dir+'/aei.txt', shell=True).wait()
 
 fa = open('for_aei.txt')
 PROC_LIMIT = 25
