8ba98b6fce6b70731a86b56ee407b9e854d3a633
Ying Peng <35216236+CongqianyouYz@users.noreply.github.com>
2020-06-24 16:55:57 +0800

    Update do_for_all_2_ret.py

--- database_analysis/do_for_all_2_ret.py
+++ database_analysis/do_for_all_2_ret.py
@@ -2,10 +2,29 @@ import subprocess
 import multiprocessing
 import os
 
-ALL_A_to_I='ALL_A_to_I.res.sorted.uniq.bed'
+with open('spe.txt') as fi: 
+    for line in fi: 
+        spe = line.strip()
+
+d = {}
+with open('/home/disk/pengying/data/formeta/'+spe+'.txt') as fi: 
+    for line in fi: 
+        seq = line.strip().split('\t')
+        if seq[0] == 'Source Name':
+            continue
+        if spe in ['hg38', 'galGal5', 'oryCun2', 'rn6']:
+            d[seq[29]] = seq[10]
+        elif spe == 'mm10':
+            d[seq[29]] = seq[6]
+        elif spe == 'rheMac8':
+            d[seq[28]] = seq[8]
+        elif spe == 'monDom5':
+            d[seq[27]] = seq[8]
 
 def Work(sprint_out_dir,a):
     print(sprint_out_dir)
+    srr = sprint_out_dir.replace('.fastq.sprint','')
+    ALL_A_to_I = 'pool_sites/cumulate.'+d[srr]+'.'+spe+'.sort.bed'
     if os.path.exists(sprint_out_dir+'/snvBAMdir/all_combined.zz.sorted.bam')==False and os.path.exists(sprint_out_dir+'/snvBAMdir/all_combined.zz.sorted')==True:
         subprocess.Popen('samtools view -H '+sprint_out_dir+'/snvBAMdir/all.bam > '+sprint_out_dir+'/snvBAMdir/SAMheader.txt',shell=True).wait()
         subprocess.Popen('python ~/scr/zz2sam.py '+sprint_out_dir+'/snvBAMdir/all_combined.zz.sorted ',shell=True).wait()
@@ -22,11 +41,11 @@ def Work(sprint_out_dir,a):
     subprocess.Popen('python ~/scr/becalled/getEL.py '+sprint_out_dir, shell=True).wait()
 
 fa = open('lst.txt')
-PROC_LIMIT = 10
+PROC_LIMIT = 20
 jobs = []
 i = 1 
 
-for line in fa: 
+for line in fa:
     sprint_out_dir = line.strip()
     print(str(i), sprint_out_dir)
     i += 1
